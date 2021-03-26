41427ed61a221c4e947ddd140afe73557628b292
chdsz123 <christian.salazar@r2devrpos.com>
2021-03-25 21:13:37 -0600

    timber

--- app/src/main/java/com/example/resourcesapp/MainActivity.kt
+++ app/src/main/java/com/example/resourcesapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var etUserName: EditText
@@ -17,6 +18,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timber.plant(Timber.DebugTree())
+
 
         bindViews()
     }
